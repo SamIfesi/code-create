d0f232b701dd02df5a8903f272d1aa9e8f79f8b1
SamIfesi <Ekesamrose@gmail.com>
2026-09-26 00:44:24 +0100

    feat: add PRIMARY_LINKS utility for navigation links

---
+++ libs/utils.ts
@@ -0,0 +1,6 @@
+export const PRIMARY_LINKS: { href: string; label: string }[] = [
+  { label: 'About us', href: '/about' },
+  { label: 'Programs', href: '/programs' },
+  { label: 'Resources', href: '/resources' },
+  { label: 'Contact', href: '/contact' },
+];
